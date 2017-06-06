0891bf8091a9248fae3b696cd056cb8171f6432c
nicobuton37 <nicobuton370@gmail.com>
2017-06-06 15:23:15 +0200

    js/css/formAdmin

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,16 @@
 <?php include "../inc/headerAdmin.php"; ?>
-    <h1>Hello, world!</h1>
+
+<div class="container-fluid">
+  <h1>Prouved me, are you Admin ?</h1>
+    <div class="form-group" id="login">
+        <form action="admin/index.php" method="post">
+          <label for="name">Login</label>
+          <input class="form-control" type="text" name="name" value="">
+          <label for="password">Password</label>
+          <input type="password" name="password" value="" class="form-control">
+        </form>
+    </div>
+</div>
+
+
 <?php include "../inc/footerAdmin.php"; ?>
---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function($){
+  $.fn.Center = function(){
+    this.css({
+      'position' : 'fixed',
+      'left' : '50%',
+      'top' : '50%'
+    });
+
+    this.css({
+      'margin-left': -this.width()/2 + 'px',
+      'marging-top': -this.height()/2 + 'px'
+    });
+  }
+})(jQuery);
+
+$(document).ready(function(){
+  $('#login').Center();
+});
